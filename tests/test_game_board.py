ec1eccab31cb15b4a25afdcae2a11634c799ef4e
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-06 14:54:29 +0200

    Implemented test_size and test_draw_pixel

--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -1,12 +1,40 @@
 import unittest
+from game_engine.board_game import BoardGame
 
 class GameBoardTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.SIZE_RAW = 2
+        self.SIZE_COLUMNE = 2
+        self.board_game = BoardGame(self.SIZE_RAW, self.SIZE_COLUMNE)
 
     def tearDown(self):
         pass
 
-    def test_default_size(self):
-        pass
+    def test_size(self):
+        size_board = 0
+        for raw in self.board_game.board_game:
+            size_board += len(raw)
 
+        self.assertEqual(
+            self.SIZE_RAW * self.SIZE_COLUMNE,
+            size_board
+        )
+
+    def test_draw_pixel(self):
+        x = 0
+        y = 0
+        representation_before_draw = '0'
+        representation_after_draw = 'x'
+        pixel = (x, y, representation_after_draw)
+
+        self.assertEqual(
+            self.board_game.board_game[x][y],
+            representation_before_draw
+        )
+
+        self.board_game.draw_pixel(pixel)
+
+        self.assertEqual(
+            self.board_game.board_game[x][y],
+            representation_after_draw
+        )
