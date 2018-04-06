643116ff06e8db6c21856f456996489708859944
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-06 13:55:05 +0200

    First implementation and added tests

---
+++ board_game.py
@@ -0,0 +1,31 @@
+class BoardGame:
+    """
+        BoardGame
+
+        pixel represenation (x, y, char)
+    """
+    def __init__(self, size_raw, size_columne):
+        self.board_game = [["0" for raw_number in range(size_raw)] for columne_number in range(size_columne)]
+
+    def print_game(self):
+        first_row = slice(1)
+        len_row = len(*self.board_game[first_row])
+        print(['-'] * (len_row + 2))
+
+        for raw in self.board_game:
+            print(['-'] + raw + ['-'])
+
+        print(['-'] * (len_row + 2))
+
+    def draw_pixel(self, pixel):
+        x, y, representation = pixel
+        self.board_game[x][y] = representation
+
+    def draw_pixels(self, pixels):
+        for x, y, representation in pixels:
+            self.board_game[x][y] = representation
+
+    def move_pixel(self, current_pixel, next_pixel):
+        cx, cy, _ = current_pixel
+        nx, ny, _ = next_pixel
+        self.board_game[cx][cy], self.board_game[nx][ny] = self.board_game[nx][ny], self.board_game[cx][cy]
---
+++ tests/test_game_board.py
@@ -0,0 +1,12 @@
+import unittest
+
+class GameBoardTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_default_size(self):
+        pass
+
