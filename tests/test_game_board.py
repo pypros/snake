f3f4bfd043aabb74e3853eb3d7ba2c1bf130bf29
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-06 15:17:34 +0200

    test_draw_pixels

--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -3,8 +3,8 @@ from game_engine.board_game import BoardGame
 
 class GameBoardTestCase(unittest.TestCase):
     def setUp(self):
-        self.SIZE_RAW = 2
-        self.SIZE_COLUMNE = 2
+        self.SIZE_RAW = 3
+        self.SIZE_COLUMNE = 3
         self.board_game = BoardGame(self.SIZE_RAW, self.SIZE_COLUMNE)
 
     def tearDown(self):
@@ -38,3 +38,28 @@ class GameBoardTestCase(unittest.TestCase):
             self.board_game.board_game[x][y],
             representation_after_draw
         )
+
+    def test_draw_pixels(self):
+        first_raw = 0
+        representation_before_draw = '0'
+        representation_after_draw = 'x'
+
+        pixels = [
+            (0, 0, representation_after_draw),
+            (0, 1, representation_after_draw),
+            (0, 2, representation_after_draw)
+        ]
+
+        for y in range(self.SIZE_COLUMNE):
+            self.assertEqual(
+                self.board_game.board_game[first_raw][y],
+                representation_before_draw
+            )
+
+        self.board_game.draw_pixels(pixels)
+
+        for y in range(self.SIZE_COLUMNE):
+            self.assertEqual(
+                self.board_game.board_game[first_raw][y],
+                representation_after_draw
+            )
