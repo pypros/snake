e1ed461943f4cf93196f0c055f752caec545fb71
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-07 08:56:34 +0200

    Removed char representation

--- game_engine/snake.py
+++ game_engine/snake.py
@@ -2,16 +2,14 @@
 class Snake:
 
     def __init__(self):
-        self.tail = (1, 1, 'x')
+        self.tail = (1, 1)
         self.body = [
-            (1, 2, 'x'),
-            (1, 3, 'x')
+            (1, 2),
+            (1, 3)
         ]
-        self.head = (1, 4, 'x')
+        self.head = (1, 4)
 
     def move(self, next_spot):
         self.body.append(self.head)
         self.head = next_spot
         self.tail = self.body.pop(0)
-
-
