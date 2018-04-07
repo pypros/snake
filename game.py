6d40995822973fdab86cfba08989b078547f4bbc
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-07 21:40:42 +0200

    Added cleaning LCD

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from game_engine.board_game import BoardGame
 from game_engine.snake import Snake
 import sys
+import os
 
 SIZE_RAW = 6
 SIZE_COLUMNE = 6
@@ -10,6 +11,7 @@ while True:
     board_game = BoardGame(SIZE_RAW, SIZE_COLUMNE)
     for x, y in snake:
         board_game.draw_pixel((x, y, 'x'))
+    os.system('clear')
     board_game.print_game()
     snake.change_direction()
     if snake.keyboard.key_value == b'\x03\x03\x03':
