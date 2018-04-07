65d6c9ed5b8566fb7855ff47cc81189353a23a06
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-07 08:59:25 +0200

    Convension test name changed and removed char representation

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -9,13 +9,13 @@ class SnakeTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_init(self):
-        tail = (1, 1, 'x')
+    def test_snake_init(self):
+        tail = (1, 1)
         body = [
-            (1, 2, 'x'),
-            (1, 3, 'x'),
+            (1, 2),
+            (1, 3),
         ]
-        head = (1, 4, 'x')
+        head = (1, 4)
 
         self.assertEqual(
             head,
@@ -32,15 +32,15 @@ class SnakeTestCase(unittest.TestCase):
             self.snake.tail
         )
 
-    def test_move_right(self):
-        tail = (1, 2, 'x')
+    def test_snake_move_right(self):
+        tail = (1, 2)
         body = [
-            (1, 3, 'x'),
-            (1, 4, 'x')
+            (1, 3),
+            (1, 4)
         ]
-        head = (1, 5, 'x')
+        head = (1, 5)
 
-        direction_right = (1, 5, 'x')
+        direction_right = (1, 5)
 
         self.snake.move(direction_right)
 
@@ -59,15 +59,15 @@ class SnakeTestCase(unittest.TestCase):
             self.snake.tail
         )
 
-    def test_move_down(self):
-        tail = (1, 2, 'x')
+    def test_snake_move_down(self):
+        tail = (1, 2)
         body = [
-            (1, 3, 'x'),
-            (1, 4, 'x')
+            (1, 3),
+            (1, 4)
         ]
-        head = (2, 4, 'x')
+        head = (2, 4)
 
-        direction_down = (2, 4, 'x')
+        direction_down = (2, 4)
 
         self.snake.move(direction_down)
 
@@ -86,14 +86,14 @@ class SnakeTestCase(unittest.TestCase):
             self.snake.tail
         )
 
-    def test_move_up(self):
-        tail = (1, 2, 'x')
+    def test_snake_move_up(self):
+        tail = (1, 2)
         body = [
-            (1, 3, 'x'),
-            (1, 4, 'x')
+            (1, 3),
+            (1, 4)
         ]
-        head = (0, 4, 'x')
-        direction_up = (0, 4, 'x')
+        head = (0, 4)
+        direction_up = (0, 4)
 
         self.snake.move(direction_up)
 
@@ -112,16 +112,16 @@ class SnakeTestCase(unittest.TestCase):
             self.snake.tail
         )
 
-    def test_move_left(self):
-        tail = (1, 3, 'x')
+    def test_snake_move_left(self):
+        tail = (1, 3)
         body = [
-            (1, 4, 'x'),
-            (2, 4, 'x')
+            (1, 4),
+            (2, 4)
         ]
-        head = (2, 3, 'x')
+        head = (2, 3)
 
-        direction_down = (2, 4, 'x')
-        direction_left = (2, 3, 'x')
+        direction_down = (2, 4)
+        direction_left = (2, 3)
 
         self.snake.move(direction_down)
         self.snake.move(direction_left)
