ea5cd964be0179cf103a0fa36c5fea6ad2ddd8b2
Lukasz Sobczuk <lukasz.sobczuk@nokia.com>
2018-04-09 21:43:14 +0200

    Eat, grow and food

--- game_engine/snake.py
+++ game_engine/snake.py
@@ -1,6 +1,6 @@
 from game_engine.keyboard_driver import Keyboard
 from game_engine.board_game import BoardGame
-
+from random import randint
 
 class Snake:
 
@@ -10,6 +10,7 @@ class Snake:
         self.head = (1, 4)
         self.keyboard = Keyboard()
         self.board_game = BoardGame(size_raw, size_columne)
+        self.food = (0, 0)
 
     def __str__(self):
         return str([self.tail] + self.body + [self.head])
@@ -28,6 +29,23 @@ class Snake:
         self.body.append(self.head)
         self.head = next_spot
         self.tail = self.body.pop(0)
+
+    def gen_food(self):
+        while True:
+            food = (
+                randint(0, self.board_game.size_raw - 1),
+                randint(0, self.board_game.size_column - 1)
+            )
+
+            if not food in ([self.tail] + self.body + [self.head]):
+                self.food = food
+                break
+
+    def eat_food(self, spot):
+        if self.food == spot:
+            self.tail = self.body.pop(0)
+            self.body = [(1, 2), (1, 3)]
+            self.head = (1, 4)
 
     def prevent_backward_movement(self, next_spot):
         if not next_spot in [self.tail] + self.body + [self.head]:
@@ -73,4 +91,8 @@ class Snake:
 
         if self.check_borders(x, y):
             if self.prevent_backward_movement((x, y)):
+                if (x, y) == self.food:
+                    self.body.append(self.head)
+                    self.head = (x, y)
+                    self.gen_food()
                 self.move((x, y))
